ee4f095ec086168f71d757122c31b2e44e22e2a6
TitlePnp <pphanupong.tle@gmail.com>
2024-05-14 23:53:40 +0700

    add logic insert from object

--- controller/product_db.js
+++ controller/product_db.js
@@ -26,10 +26,20 @@ exports.getProductID = (req, res) => {
     });
 };
 
+// exports.addProduct = (req, res) => {
+//     const productDetail = req.body;
+//     const sql = `INSERT INTO products(product_name, product_image, product_description, product_price, product_quantity, product_status, type_id) VALUES(?,?,?,?,?,?,?)`;
+//     connection.query(sql, [productDetail.product_name, productDetail.product_image, productDetail.product_description, productDetail.product_price, productDetail.product_quantity, productDetail.product_status, productDetail.type_id], (err, result) => {
+//         if (err) throw err;
+//         res.status(201).send(result);
+//     }
+//     );
+// };
+
 exports.addProduct = (req, res) => {
     const productDetail = req.body;
     const sql = `INSERT INTO products(product_name, product_image, product_description, product_price, product_quantity, product_status, type_id) VALUES(?,?,?,?,?,?,?)`;
-    connection.query(sql, [productDetail.product_name, productDetail.product_image, productDetail.product_description, productDetail.product_price, productDetail.product_quantity, productDetail.product_status, productDetail.type_id], (err, result) => {
+    connection.query(sql, Object.values(productDetail), (err, result) => {
         if (err) throw err;
         res.status(201).send(result);
     }
